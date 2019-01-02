af785a5df245d45447df9a7f1f889165e8ba7364
Adam Swiezowski <adam.swiezowski+git@gmail.com>
2019-01-02 19:39:03 +0100

    calculate rpn value support for floats

--- reverse_polish_notation.py
+++ reverse_polish_notation.py
@@ -1,48 +1,77 @@
 
 import re
-from functools import reduce 
+from functools import reduce
 
 class Element:
 
     def __init__(self, value):
         self._value = value
-    
+
     @property
     def value(self):
         return self._value
-    
+
     def __repr__(self):
         return self._value
 
     def __eq__(self, other):
         return self.value == other.value
 
+
 class NumberElement(Element):
 
     def __init__(self, number_value):
-        Element.__init__(self,number_value)
+        Element.__init__(self, number_value)
 
-class OpertatorElement(Element):
+    def numeric_value(self):
+        return float(self.value)
+
+
+class OperatorElement(Element):
+
+    operators_functions = {"^": lambda x, y: x ** y,
+                           "*": lambda x, y: x * y,
+                           "/": lambda x, y: x / y,
+                           "%": lambda x, y: x % y,
+                           "+": lambda x, y: x + y,
+                           "-": lambda x, y: x - y}
 
     def __init__(self, value):
         Element.__init__(self, value)
+    
+    def calculate(self, x, y):
+        return self.operators_functions[self.value](x, y)
+
 
 class ReversePolishNotationExpression:
 
     def __init__(self, rpn):
         self.rpn = rpn
-    
+
     @property
     def value(self):
         return self.rpn
 
+    def calculate(self):
+        stack = []
+        for element in self.rpn:
+            if isinstance(element, OperatorElement):
+                x = stack.pop()
+                y = stack.pop()
+                result = element.calculate(y,x)
+                stack.append(result)
+            else:
+                stack.append(element.numeric_value())
+        return stack.pop()
+
     def __repr__(self):
-        return reduce(lambda x,y: str(x) + " " + str(y), self.rpn)
+        return reduce(lambda x, y: str(x) + " " + str(y), self.rpn)
+
 
 class AlgebraicExpressionParser:
 
     operators = {"^": 3, "*": 2, "/": 2, "%": 2, "+": 1, "-": 1, "(": 0}
-    digit_pattern = re.compile("\\w")
+    digit_pattern = re.compile("[\\w\\.]")
 
     def __init__(self, algebraic_expression):
         self.algebraic_expression = algebraic_expression
@@ -61,24 +90,26 @@ class AlgebraicExpressionParser:
                 operators_stack.append(element)
             elif element == ")":
                 while operators_stack[-1] != "(":
-                    elements.append(OpertatorElement(operators_stack.pop()))
+                    elements.append(OperatorElement(operators_stack.pop()))
                 operators_stack.pop()
             elif element in self.operators:
                 o1 = element
                 while len(operators_stack) > 0 and self.operators[o1] <= self.operators[operators_stack[-1]]:
-                    elements.append(OpertatorElement(operators_stack.pop()))
+                    elements.append(OperatorElement(operators_stack.pop()))
                 operators_stack.append(o1)
 
-        if len(numeric_symbol)!= 0:
+        if len(numeric_symbol) != 0:
             elements.append(NumberElement(numeric_symbol))
         while len(operators_stack) > 0:
             operator = operators_stack.pop()
             if operator == "(":
                 print("Incorrect expression syntax. End brace not found")
-            elements.append(OpertatorElement(operator))
+            elements.append(OperatorElement(operator))
         return ReversePolishNotationExpression(elements)
+
 
 if __name__ == '__main__':
     algebraic_expression = input("Enter algebraic expression: ")
-    rpn_expression = AlgebraicExpressionParser(algebraic_expression).convert_to_rpn()
+    rpn_expression = AlgebraicExpressionParser(
+        algebraic_expression).convert_to_rpn()
     print(rpn_expression)
--- test_reverse_polish_notation.py
+++ test_reverse_polish_notation.py
@@ -1,29 +1,40 @@
 import unittest
 
-from reverse_polish_notation import AlgebraicExpressionParser, NumberElement, OpertatorElement
+from reverse_polish_notation import AlgebraicExpressionParser, NumberElement, OperatorElement
 
 
 class AlgebraicExpressionParserCase(unittest.TestCase):
 
     def test_adding_two_symbols(self):
         rpn = AlgebraicExpressionParser("a+b").convert_to_rpn()
-        self.assertEqual(rpn.value, [NumberElement("a"), NumberElement("b"), OpertatorElement("+")])
+        self.assertEqual(rpn.value, [NumberElement("a"), NumberElement("b"), OperatorElement("+")])
 
     def test_add_subtract_three_symbols(self):
         rpn = AlgebraicExpressionParser("a+b-c").convert_to_rpn()
-        self.assertEqual(rpn.value, [NumberElement("a"), NumberElement("b"), OpertatorElement("+"), NumberElement("c"), OpertatorElement("-")])
+        self.assertEqual(rpn.value, [NumberElement("a"), NumberElement("b"), OperatorElement("+"), NumberElement("c"), OperatorElement("-")])
 
     def test_add_multiply_three_symbols(self):
         rpn = AlgebraicExpressionParser("a+b*c").convert_to_rpn()
         self.assertEqual(rpn.value, [NumberElement("a"), NumberElement(
-            "b"), NumberElement("c"), OpertatorElement("*"), OpertatorElement("+")])
+            "b"), NumberElement("c"), OperatorElement("*"), OperatorElement("+")])
 
     def test_add_multiply_paranthesis_three_symbols(self):
         rpn = AlgebraicExpressionParser("(a+b)*c").convert_to_rpn()
-        self.assertEqual(rpn.value, [NumberElement("a"), NumberElement("b"), OpertatorElement("+"), 
-        NumberElement("c"), OpertatorElement("*")])
+        self.assertEqual(rpn.value, [NumberElement("a"), NumberElement("b"), OperatorElement("+"), 
+        NumberElement("c"), OperatorElement("*")])
 
     def test_complex_expression(self):
         rpn = AlgebraicExpressionParser("12 + a * (b * c + d / e)").convert_to_rpn()
-        self.assertEqual(rpn.value, [NumberElement("12"), NumberElement("a"), NumberElement("b"), NumberElement("c"), OpertatorElement("*"), 
-        NumberElement("d"), NumberElement("e"), OpertatorElement("/"), OpertatorElement("+"), OpertatorElement("*"), OpertatorElement("+")])
+        self.assertEqual(rpn.value, [NumberElement("12"), NumberElement("a"), NumberElement("b"), NumberElement("c"), OperatorElement("*"), 
+        NumberElement("d"), NumberElement("e"), OperatorElement("/"), OperatorElement("+"), OperatorElement("*"), OperatorElement("+")])
+
+    def test_parsing_floats(self):
+        rpn = AlgebraicExpressionParser("23.456-78.09").convert_to_rpn()
+        self.assertEqual(rpn.value, [NumberElement("23.456"), NumberElement("78.09"), OperatorElement("-")])
+    def test_calculate_adding(self):
+        rpn = AlgebraicExpressionParser("2.1+1.9").convert_to_rpn()
+        self.assertEqual(rpn.calculate(), 4)
+
+    def test_calculate_priority(self):
+        rpn = AlgebraicExpressionParser("2-5%4+4*(5-1)/2^3").convert_to_rpn()
+        self.assertEqual(rpn.calculate(), 3)
